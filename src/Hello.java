fa79bb085516f18374a855a77134b3f7a1e01872
Siddhant Wankhade <siddhantwankhade23@gmail.com>
2022-11-13 11:36:08 +0530

    feature 1

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+
+    private void displayFeature1(){
+        System.out.println("Feature 1");
+    }
 }
